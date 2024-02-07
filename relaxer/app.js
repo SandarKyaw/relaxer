8ac7afb8b303035c19337cb9d2c4bbe42c4b1f86
SandarKyaw <44694787+SandarKyaw@users.noreply.github.com>
2024-02-07 20:18:29 +0630

    Add files via upload

---
+++ relaxer/app.js
@@ -0,0 +1,17 @@
+const text = document.querySelector("#text");
+const container = document.querySelector(".container");
+
+const start = () => {
+    text.textContent = "Breath In";
+    container.className = "container grow";
+    setTimeout(() => {
+        text.textContent="Hold";
+    setTimeout(()=> {
+        text.textContent="Breath Out";
+        container.className="container drop";
+    }, 1500)
+    }, 3000)
+}
+
+start();
+setInterval(start, 7500);
